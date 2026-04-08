4cf6943e79426b9f2fa0dd12b813cdaaaf384c9e
BOUYAKNIFEN Zakariae <zikobouy@gmail.com>
2026-04-08 20:53:05 +0100

    M6

--- isometric-game-demo/games/frogger/progress/12 transition/scenes/global.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-var score: int = 10
--- isometric-game-demo/games/frogger/progress/13 sound/scenes/title.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _ready() -> void:
-	$Label2.text = 'High score: ' + str(Global.score)
-
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("confirm"):
-		get_tree().change_scene_to_file("res://games/frogger/scenes/game.tscn")
